b56870819aac789607c783f5a57cc1ea0949047a
sandeepreddy1945 <b.sandeepreddy209@gmail.com>
2024-12-20 22:01:11 +0530

    Initial Commit of Fully Developed Code
    Signed-off-by: sandeepreddy1945 <b.sandeepreddy209@gmail.com>

---
+++ src/navigation.ts
@@ -0,0 +1,123 @@
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    {
+      text: 'Home',
+      href: getPermalink('/'),
+    },
+    {
+      text: 'More',
+      links: [
+        {
+          text: 'About us',
+          href: getPermalink('/#aboutUs'),
+        },
+        {
+          text: 'Services',
+          href: getPermalink('/services'),
+        },
+        {
+          text: 'FAQ',
+          href: getPermalink('/faq'),
+        },
+        {
+          text: 'Progress Since 2021',
+          href: getPermalink('/progress'),
+        },
+        {
+          text: 'Selection Process',
+          href: getPermalink('/process'),
+        },
+        {
+          text: 'Contact',
+          href: getPermalink('/contact'),
+        },
+        {
+          text: 'Terms',
+          href: getPermalink('/terms'),
+        },
+        {
+          text: 'Privacy policy',
+          href: getPermalink('/privacy'),
+        },
+      ],
+    },
+    {
+      text: 'Aaptha Stories',
+      links: [
+        {
+          text: 'Client Testimonials',
+          href: getPermalink('/clientTestimonials'),
+        },
+        {
+          text: 'Blog List',
+          href: getBlogPermalink(),
+        },
+      ],
+    },
+    {
+      text: 'Careers',
+      href: getPermalink('/careers'),
+    },
+  ],
+  actions: [{ text: 'Contact Us', href: getPermalink('/contact') }],
+};
+
+export const footerData = {
+  links: [
+    {
+      title: 'Company',
+      links: [
+        { text: 'Home', href: getPermalink('/') },
+        { text: 'About', href: getPermalink('/#aboutUs') },
+        { text: 'Blog', href: getBlogPermalink() },
+        { text: 'Careers', href: getPermalink('/contact') },
+        { text: 'Services', href: getPermalink('/services') },
+      ],
+    },
+    {
+      title: 'Support',
+      links: [
+        { text: 'Contact', href: getPermalink('/contact') },
+        { text: 'Register', href: getPermalink('/contact') },
+        { text: 'FAQ', href: getPermalink('/faq') },
+      ],
+    },
+  ],
+  secondaryLinks: [
+    { text: 'Terms', href: getPermalink('/terms') },
+    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+  ],
+  socialLinks: [
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://twitter.com/AapthaC', target: '_blank' },
+    {
+      ariaLabel: 'Instagram',
+      icon: 'tabler:brand-instagram',
+      href: 'https://www.instagram.com/aapthacare',
+      target: '_blank',
+    },
+    {
+      ariaLabel: 'Facebook',
+      icon: 'tabler:brand-facebook',
+      href: 'https://www.facebook.com/Aapthaeldercare',
+      target: '_blank',
+    },
+    {
+      ariaLabel: 'X',
+      icon: 'tabler:brand-linkedin',
+      href: 'https://www.linkedin.com/company/aaptha-eldercare-service',
+      target: '_blank',
+    },
+    {
+      ariaLabel: 'Github',
+      icon: 'tabler:brand-github',
+      href: 'https://github.com/onwidget/astrowind',
+      target: '_blank',
+    },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+  ],
+  footNote: `
+    ©2021 by Aaptha Elder-Care Services · All rights reserved.
+  `,
+};
---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import plugin from 'tailwindcss/plugin';
+import typographyPlugin from '@tailwindcss/typography';
+
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--aw-color-primary)',
+        secondary: 'var(--aw-color-secondary)',
+        accent: 'var(--aw-color-accent)',
+        default: 'var(--aw-color-text-default)',
+        muted: 'var(--aw-color-text-muted)',
+        stroke: "#EEEEEE",
+        strokedark: "#2D2F40",
+      },
+      fontFamily: {
+        sans: ['var(--aw-font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
+        serif: ['var(--aw-font-serif, ui-serif)', ...defaultTheme.fontFamily.serif],
+        heading: ['var(--aw-font-heading, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
+      },
+
+      animation: {
+        fade: 'fadeInUp 1s both',
+        line1: "line 3s linear infinite",
+        line2: "line 6s linear infinite",
+        line3: "line 9s linear infinite",
+      },
+
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: 0, transform: 'translateY(2rem)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        line: {
+          '0%, 100%': { transform: 'translateY(100%)' },
+          '50%': { transform: 'translateY(0)' },
+        },
+        
+      },
+    },
+  },
+  plugins: [
+    typographyPlugin,
+    plugin(({ addVariant }) => {
+      addVariant('intersect', '&:not([no-intersect])');
+    }),
+  ],
+  darkMode: 'class',
+};
