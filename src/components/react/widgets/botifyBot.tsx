3848bbaa317ca60047d92cac34aa258407034095
sandeepreddy1945 <b.sandeepreddy209@gmail.com>
2024-12-23 22:33:25 +0530

    Added custom bot properties
    Signed-off-by: sandeepreddy1945 <b.sandeepreddy209@gmail.com>

--- src/components/react/widgets/botifyBot.tsx
+++ src/components/react/widgets/botifyBot.tsx
@@ -1,4 +1,5 @@
 import { lazy, Suspense, useEffect, useState } from 'react';
+import roboticon from '~/assets/favicons/roboticon.svg';
 
 const ChatBot = lazy(() => import('react-chatbotify'));
 
@@ -7,12 +8,35 @@ export default function BotifyBot() {
   useEffect(() => {
     setIsLoaded(true);
   }, []);
-
+  const id = 'my-chatbot-id';
+  const flow = {
+    start: {
+      message: 'Hello there!',
+      path: 'end',
+    },
+    end: {
+      message: 'See you, goodbye!',
+    },
+  };
   return (
     <>
       {isLoaded && (
         <Suspense fallback={<div>Loading...</div>}>
-          <ChatBot />
+          <ChatBot
+            id={id}
+            flow={flow}
+            settings={{
+              device: { mobileEnabled: true, applyMobileOptimizations: true },
+              footer: { text: 'Aaptha Elder Care. Here to Help!' },
+              header: { title: 'Aaptha Elder Care' },
+              botBubble: { showAvatar: true },
+              chatWindow: { showMessagePrompt: false },
+              tooltip: { mode: 'NEVER' },
+              notification: { showCount: false },
+              chatButton: { icon: roboticon.src },
+              voice: { disabled: true },
+            }}
+          />
         </Suspense>
       )}
     </>
